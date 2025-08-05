191c9ceed08494c2b55fcba52c49709f9b79d54d
hadimalik2005 <hadia7549@gmail.com>
2025-08-05 19:49:37 -0400

    changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig
+module.exports = {
+  basePath: isProd ? '/hadimalik-portfolio' : '',
+  assetPrefix: isProd ? '/hadimalik-portfolio/' : '',
+  trailingSlash: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
