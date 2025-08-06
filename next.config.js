b9da8d388b08904c206a4e25a03dcaa0a8d8525f
hadimalik2005 <hadia7549@gmail.com>
2025-08-05 20:34:36 -0400

    updates to base path

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  basePath: isProd ? '/hadimalik-portfolio' : '',
-  assetPrefix: isProd ? '/hadimalik-portfolio/' : '',
-  trailingSlash: true,
   output: 'export',
   images: {
     unoptimized: true,
